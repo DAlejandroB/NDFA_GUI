27d2e64d184ab979055cc43f88f9c1a4d250e5b7
Julian Moyano <51031164+OminousJamm@users.noreply.github.com>
2022-03-23 19:42:19 -0500

    validar palabra
    se agrego un mensaje al validar la palabra

--- src/controller/ControlAutomata.java
+++ src/controller/ControlAutomata.java
@@ -9,48 +9,61 @@ import javax.swing.JOptionPane;
 
 import views.main_frame.MainFrame;
 
-public class ControlAutomata implements ActionListener{
+public class ControlAutomata implements ActionListener {
 
-    private MainFrame frame; //instancia de la vista, frame principal
-    private NDFA finiteAutomata = new NDFA();
-    public ControlAutomata(){
-        frame = new MainFrame(this);
-    }
+	private static final String VALIDATION_FAILED = "No se pudo validar Correctamente la palabra";
+	private static final String VALIDATION_SUCESS = "Se valido Correctamente la palabra";
+	private MainFrame frame; // instancia de la vista, frame principal
+	private NDFA finiteAutomata = new NDFA();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        switch(Commands.valueOf(e.getActionCommand())){
-            case C_RESTART:
-                // reiniciar el automata 
-                frame.restartAutomaton();
-                break;
-            case C_VALIDATE_WORD:
-                // jOption para ingresar la palabra
-                this.validateWord();
-                break;
-            default:
-                break;
+	public ControlAutomata() {
+		frame = new MainFrame(this);
+	}
 
-        }
-        
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (Commands.valueOf(e.getActionCommand())) {
+		case C_RESTART:
+			// reiniciar el automata
+			frame.restartAutomaton();
+			break;
+		case C_VALIDATE_WORD:
+			// jOption para ingresar la palabra
+			this.validateWord();
+			break;
+		default:
+			break;
 
-    public void validateWord() {
-		String name = JOptionPane.showInputDialog("Ingresa la palabra a validar"); 
-		if (JOptionPane.showConfirmDialog(frame, "¿Seguro que desea validar esta palabra: " + name + "?",
-				"Pregunta", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-			// llamar al metodo de validacion del modelo y enviar la palabra guardada en name
-            System.out.println(finiteAutomata.validateWord(name));
+		}
+
+	}
+
+	public void validateWord() {
+		String name = JOptionPane.showInputDialog("Ingresa la palabra a validar");
+		if (JOptionPane.showConfirmDialog(frame, "¿Seguro que desea validar esta palabra: " + name + "?", "Pregunta",
+				JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			// llamar al metodo de validacion del modelo y enviar la palabra guardada en
+			// name
+			boolean validation = finiteAutomata.validateWord(name);
+			if (validation) {
+				this.frame.showMessage(VALIDATION_SUCESS);
+			} else {
+				this.frame.showMessage(VALIDATION_FAILED);
+			}
+
 		}
 	}
 
-    public void addState(String stateTag) {
-    	finiteAutomata.addState(stateTag, StateType.DEFAULT);
-    }
-    public void updateState(String stateTag, StateType type) {
-    	finiteAutomata.getStates().get(finiteAutomata.getStateIndex(stateTag)).setType(type);
-    }
-    public void addTransition(String startTag, String endTag, char condition) {
-    	finiteAutomata.addTransition(condition, finiteAutomata.getStates().get(finiteAutomata.getStateIndex(startTag)), finiteAutomata.getStates().get(finiteAutomata.getStateIndex(endTag)));
-    }
+	public void addState(String stateTag) {
+		finiteAutomata.addState(stateTag, StateType.DEFAULT);
+	}
+
+	public void updateState(String stateTag, StateType type) {
+		finiteAutomata.getStates().get(finiteAutomata.getStateIndex(stateTag)).setType(type);
+	}
+
+	public void addTransition(String startTag, String endTag, char condition) {
+		finiteAutomata.addTransition(condition, finiteAutomata.getStates().get(finiteAutomata.getStateIndex(startTag)),
+				finiteAutomata.getStates().get(finiteAutomata.getStateIndex(endTag)));
+	}
 }
--- src/views/main_frame/NorthPanel.java
+++ src/views/main_frame/NorthPanel.java
@@ -8,17 +8,16 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import controller.Commands;  
+import controller.Commands;
 
-public class NorthPanel extends JPanel{    
-    /**
+public class NorthPanel extends JPanel {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JButton restartAutomaton, validateWord;
 
-
-	public NorthPanel(ActionListener listener){
+	public NorthPanel(ActionListener listener) {
 		restartAutomaton = new JButton();
 		validateWord = new JButton();
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -27,15 +26,15 @@ public class NorthPanel extends JPanel{
 		this.setVisible(true);
 	}
 
-    private void initComponents(ActionListener actionListener){
-        restartAutomaton.setText("Reiniciar");
-        restartAutomaton.addActionListener(actionListener);
-        restartAutomaton.setActionCommand(Commands.C_RESTART.toString());
-        this.add(restartAutomaton);
-        validateWord.addActionListener(actionListener);
-        validateWord.setActionCommand(Commands.C_VALIDATE_WORD.toString());
-        validateWord.setText("Ingresar palabra");
-        this.add(validateWord);
-    }
+	private void initComponents(ActionListener actionListener) {
+		restartAutomaton.setText("Reiniciar");
+		restartAutomaton.addActionListener(actionListener);
+		restartAutomaton.setActionCommand(Commands.C_RESTART.toString());
+		this.add(restartAutomaton);
+		validateWord.addActionListener(actionListener);
+		validateWord.setActionCommand(Commands.C_VALIDATE_WORD.toString());
+		validateWord.setText("Ingresar palabra");
+		this.add(validateWord);
+	}
 
 }
